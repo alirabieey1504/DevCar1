124bb637768e38abbca1a61c84eae83ffca967e0
alirabieey1504 <alidevops1504@gmail.com>
2023-07-12 13:17:38 +0430

    index & contact page added

--- DevCar1_MVC/Controllers/HomeController.cs
+++ DevCar1_MVC/Controllers/HomeController.cs
@@ -1,21 +1,16 @@
 ﻿using DevCar1_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCar1_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+           
         }
 
         public IActionResult Index()
@@ -23,10 +18,12 @@ namespace DevCar1_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
