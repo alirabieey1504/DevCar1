e20848b2e2d77c0580c603893ca2603c8f65c9a2
alirabieey1504 <alidevops1504@gmail.com>
2023-07-12 15:16:09 +0430

    Project section index page moved to partial view Project

---
+++ DevCar1_MVC/Controllers/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace DevCar1_MVC.Controllers
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Discription{ get; set; }
+    }
+}
---
+++ DevCar1_MVC/Controllers/IndexModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DevCar1_MVC.Controllers
+{
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
